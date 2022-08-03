d9cc56c2dfd58201e6a4e61f1a8bb6f71a480fb8
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2022-08-03 02:12:39 +0100

    Record component

---
+++ components/Record/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+export default function Record() {
+    return (
+        <div className={styles.content}>
+            <div className={styles.left}>
+                <h3>Your <del>record studio</del> is going to change the game, right?</h3>
+            </div>
+            <div className={styles.right}>
+                <p>Massa, aliquam odio amet eget mollis pellentesque. Tellus mi diam rhoncus facilisis sed. Eus facilisi nulla pellentesquetus. Fames mauris amet nunc, mauris, porttitor duis venenatis. Posuere dictum elit, dictum purus.</p>
+                <button>Read more</button>
+            </div>
+        </div>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Navbar from "../components/Navbar/index"
 import Header from "../components/Header/index"
+import Record from "../components/Record/index"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Header />
+      <Record />
     </div>
   )
 }
