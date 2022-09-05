05bee8c2d2769e25c1d30d38fafac453f24d7cd5
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2022-09-05 18:58:45 +0100

    Responsiveness update

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
       <div className="Wide">
         <Header />
         <Record />
-        {/*<Lyrics />
-        <Info />
+        <Lyrics />
+        {/*<Info />
         <Location />
         <Contact />*/}
       </div>
